197cd57ebee803e5dd058a0808f735f8a25d035c
Denis Kisina <kisinad@gmail.com>
2020-04-25 22:20:28 -0500

    finished styling

--- js/app.js
+++ js/app.js
@@ -7,8 +7,24 @@ function showTime() {
     let hour = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+    if (hour === 0){
+        hour = 12;
+    }
+    hour = (hour < 10)? "0" + hour : hour;
+    minutes = (minutes < 10)? "0" + minutes : minutes;
+    seconds = (seconds < 10)? "0" + seconds : seconds;
 
-    let time = hour + ":" + ":" + minutes + ":" + seconds;
-    document.getElementById("time").innerHTML = time;
+    let time = hour + ":"  + minutes + ":" + seconds;
+    let calenderDay = day + "/" + month + "/" + year;
 
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    document.getElementById("days").innerHTML = day;
+    document.getElementById("month").innerHTML = month;
+    document.getElementById("year").innerHTML = year;
+    document.getElementById("sound").play
+    setTimeout(showTime, 1000);
 }
+showTime();
